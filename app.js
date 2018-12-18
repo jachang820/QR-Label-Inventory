9bc5c0d6e5a408454a024db1aad9ab008f8accdc
Jonathan Chang <j.a.chang820@gmail.com>
2018-12-18 11:30:31 -0800

    Jon/auth0 integration (#7)
    * Added .env to .gitignore, since it contains client secret.
    
    * Added auth0 social and passwordless login support.
    
    * merged with database changes
    
    * Added userInViews.js
    
    * fixed email in dashboard
    
    * added email and name for real this time

--- app.js
+++ app.js
@@ -3,9 +3,42 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var dotenv = require('dotenv');
+var passport = require('passport');
+var Auth0Strategy = require('passport-auth0');
 
+var userInViews = require('./lib/middleware/userInViews');
+var authRouter = require('./routes/auth');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+dotenv.load();
+
+// configure passport to use auth0
+var strategy = new Auth0Strategy(
+	{
+		domain: process.env.AUTH0_DOMAIN,
+		clientID: process.env.AUTH0_CLIENT_ID,
+		clientSecret: process.env.AUTH0_CLIENT_SECRET,
+		callbackURL:
+			process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+	},
+	function (accessToken, refreshToken, extraParams, profile, done) {
+		return done(null, profile)
+	}
+);
+
+passport.use(strategy);
+
+passport.serializeUser(function (user, done) {
+	done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+	done(null, user);
+})
+
 
 var app = express();
 
@@ -19,6 +52,25 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// config session cookie
+var sess = {
+	secret: '7B1A5824E9F9F',
+	cookie: {},
+	resave: false,
+	saveUninitialized: true
+};
+
+if (app.get('env') === 'production') {
+	session.cookie.secure = true; // require https
+}
+
+app.use(session(sess));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(userInViews());
+app.use('/', authRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
