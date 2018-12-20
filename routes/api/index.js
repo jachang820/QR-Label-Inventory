b217392bc9b371058d863b881f765ee617f66ac6
alexanderqchen <aqchen@ucla.edu>
2018-12-20 11:58:16 -0800

    Created items-api

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
+<<<<<<< HEAD
 const itemsRouter = require('./items');
 const usersRouter = require('./users');
 
 router.use('/items', itemsRouter);
 router.use('/users', usersRouter);
+=======
+
+const items = require('./items');
+const users = require('./users');
+
+router.use('/items', items);
+router.use('/users', users);
+>>>>>>> Created items-api
 
 module.exports = router;
