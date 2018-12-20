b1f28715a181c86916be84e8348b62a20cb77658
Jonathan Chang <j.a.chang820@gmail.com>
2018-12-20 13:19:30 -0800

    added back middleware

---
+++ middleware/secured.js
@@ -0,0 +1,8 @@
+// go to login page if user is not logged in
+module.exports = function() {
+	return function secured (req, res, next) {
+		if (req.user) { return next(); }
+		req.session.returnTo = req.originalUrl;
+		res.redirect('/login')
+	}
+}
