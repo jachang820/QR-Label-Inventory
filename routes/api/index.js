92011a0852406d00851ac33d81fdd088787102b7
alexanderqchen <aqchen@ucla.edu>
2018-12-20 20:19:38 -0800

    Created api for colors and sizes
    Issue: https://github.com/jachang820/smokebuddy/issues/30

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const colorsRouter = require('./colors');
 const itemsRouter = require('./items');
+const sizesRouter = require('./sizes');
 const usersRouter = require('./users');
 
+router.use('/colors', colorsRouter);
 router.use('/items', itemsRouter);
+router.use('/sizes', sizesRouter);
 router.use('/users', usersRouter);
 
 module.exports = router;
