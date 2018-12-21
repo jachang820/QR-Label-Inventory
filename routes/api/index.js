5321ed7bb8fc86cd3b3faffd230040c792ce5d3f
Alexander Chen <aqchen@ucla.edu>
2018-12-20 20:12:21 -0800

    Created factory orders api (#22)
    Issue: https://github.com/jachang820/smokebuddy/issues/20

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const factoryOrdersRouter = require('./factory_orders');
 const itemsRouter = require('./items');
 const usersRouter = require('./users');
 
+router.use('/factory_orders', factoryOrdersRouter);
 router.use('/items', itemsRouter);
 router.use('/users', usersRouter);
 
