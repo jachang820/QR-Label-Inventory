c64c046fd1a57d7b530ede698c100b4ce653ed68
alexanderqchen <aqchen@ucla.edu>
2018-12-24 07:17:07 -0800

    Finished api to scan out items

--- routes/api/scan.js
+++ routes/api/scan.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const { Items, FactoryOrder } = require('../../models')
+const { Items, FactoryOrders } = require('../../models')
 
 const markInStock = async (items) => {
   for (const item of items) {
     await item.update({ status: 'In Stock'});
+
+    FactoryOrders.findOne({ where: { id: item.FactoryOrderId }})
+    .then((factoryOrder) => {
+      if (!factoryOrder.arrival_date) {
+        factoryOrder.update({ arrival_date: Date.now() })
+      }
+    })
+  }
+};
+
+const markShippedWithCustomerOrder = async (items, CustomerOrderId) => {
+  for (const item of items) {
+    await item.update({ status: 'Shipped', CustomerOrderId });
   }
 };
 
@@ -39,7 +52,7 @@ router.post('/in/:id', (req, res, next) => {
           return;
         }
 
-        res.status(400).send('Item not found.');
+        res.json();
       })
     })
   })
@@ -47,8 +60,42 @@ router.post('/in/:id', (req, res, next) => {
 });
 
 // Scan items out of the warehouse
-router.post('/out', (req, res, next) => {
+router.post('/out/:id', (req, res, next) => {
+  const id = req.params.id;
+  const CustomerOrderId = req.body.CustomerOrdersId;
 
+  // Assume id is outerbox
+  Items.findAll({ where: { outerbox: id }})
+  .then(async (items) => {
+    if (items.length > 0) {
+      await markShippedWithCustomerOrder(items, CustomerOrderId);
+      res.json(items);
+      return;
+    }
+
+    // Assumd id is innerbox
+    Items.findAll({ where: { innerbox: id }})
+    .then(async (items) => {
+      if (items.length > 0) {
+        await markShippedWithCustomerOrder(items, CustomerOrderId);
+        res.json(items);
+        return;
+      }
+
+      // Assume id is item
+      Items.findOne({ where: { id }})
+      .then(async (item) => {
+        if (item) {
+          await markShippedWithCustomerOrder([item], CustomerOrderId);
+          res.json([item]);
+          return;
+        }
+
+        res.json();
+      })
+    })
+  })
+  .catch(next);
 });
 
 module.exports = router;
