7bc465997a89319d1f5ede5f063c52f9e3b85c05
Jonathan Chang <j.a.chang820@gmail.com>
2018-12-30 11:26:14 -0800

    organized script and made it easier to make database changes

--- scripts/dummy-data.js
+++ scripts/dummy-data.js
@@ -1,113 +1,78 @@
 const { Colors, Sizes, FactoryOrders,
   CustomerOrders, Items, Users } = require('../models');
+const uuidv4 = require('uuid/v4');
 
 async function setup() {
-  await Colors.findOrCreate({ where: {
-    name: 'blue'
-  }});
 
-  await Colors.findOrCreate({ where: {
-    name: 'red'
-  }});
+  await Colors.bulkCreate([
+    { name: 'blue' },
+    { name: 'red' },
+    { name: 'glow-in-the-dark' },
+    { name: 'Halloween' }
+  ]);
 
-  await Sizes.findOrCreate({ where: {
-    name: 'jr'
-  }});
+  colors = await Colors.findAll();
+  colors = colors.map((e) => { return e.name; });
 
-  await Sizes.findOrCreate({ where: {
-    name: 'regular'
-  }});
+  await Sizes.bulkCreate([
+    { name: 'junior' },
+    { name: 'original' },
+    { name: 'MEGA' }
+  ]);
 
-  await FactoryOrders.findOrCreate({ where: {
-    id: 1
-  }});
+  sizes = await Sizes.findAll();
+  sizes = sizes.map((e) => { return e.name });
 
-  await FactoryOrders.findOrCreate({ where: {
-    id: 2
-  }});
+  await FactoryOrders.bulkCreate([
+    { id: 1 },
+    { id: 2 }
+  ]);
 
-  await CustomerOrders.findOrCreate({ where: {
-    id: 'aee7514d-f202-4f9d-8212-97d9a459cbda'
-  }});
+  await CustomerOrders.bulkCreate([ 
+    { id: 'aee7514d-f202-4f9d-8212-97d9a459cbda' },
+    { id: '030837e2-b120-4359-8d16-e4adab83004a' }
+  ]);
 
-  await CustomerOrders.findOrCreate({ where: {
-    id: '030837e2-b120-4359-8d16-e4adab83004a'
-  }});
+  await Users.bulkCreate([
+    { firstname: 'Jonathan',
+      lastname: 'Chang',
+      email: 'j.a.chang820@gmail.com',
+      role: 'Administrator'},
+    { firstname: 'Alex',
+      lastname: 'Chen',
+      email: 'aqchen@g.ucla.edu',
+      role: 'Administrator' }
+  ]);
 
-  /**********
-   * OUTER BOX 1
-   */
+  let items = []
+  for (let i = 0; i < 72; i++) {
+    let color = colors[i % 4];
+    let size = sizes[i % 3];
+    if (i % 12 == 0) {
+      var inner = uuidv4();
+      if (i % 48 == 0) {
+        var master = uuidv4();
+      }
+    }
 
-  // Inner box 1
-  await Items.findOrCreate({ where: {
-    id: '89938a6e-8310-41c1-a62f-89b5b2cdeae0',
-    status: 'Ordered',
-    innerbox: 'c9ca6fff-1ef7-44a7-ad28-60f31672ef0e',
-    outerbox: '993ea7e9-6185-4207-b5d8-5f994c6fd806',
-    ColorName: 'blue',
-    SizeName: 'jr',
-    FactoryOrderId: 1
-  }});
+    items.push({
+      id: uuidv4(),
+      status: 'Ordered',
+      innerbox: inner,
+      outerbox: master,
+      ColorName: color,
+      SizeName: size
+    });
+  }
 
-  await Items.findOrCreate({ where: {
-    id: '20173bd2-02be-418e-85f9-be012b9ed3ae',
-    status: 'Shipped',
-    innerbox: 'c9ca6fff-1ef7-44a7-ad28-60f31672ef0e',
-    outerbox: '993ea7e9-6185-4207-b5d8-5f994c6fd806',
-    ColorName: 'blue',
-    SizeName: 'jr',
-    FactoryOrderId: 1,
-    CustomerOrderId: '030837e2-b120-4359-8d16-e4adab83004a' // 2
-  }});
+  await Items.bulkCreate(items);
 
-  // Inner box 2
-  await Items.findOrCreate({ where: {
-    id: 'c4d657d9-c16e-4f97-b940-c3f3f45b695e',
-    status: 'In Stock',
-    innerbox: '90ec6e3f-98a2-44e6-9b9f-9b5c5a7bd3b2',
-    outerbox: '993ea7e9-6185-4207-b5d8-5f994c6fd806',
-    ColorName: 'red',
-    SizeName: 'jr',
-    FactoryOrderId: 1
-  }});
-
-  /**********
-   * OUTER BOX 2
-   */
-
-  // Inner box 1
-  await Items.findOrCreate({ where: {
-    id: 'c4896897-b8fb-4404-9fe1-aa32b1e40a9b',
-    status: 'Shipped',
-    innerbox: '649279f7-675e-4f8f-a94a-bff37a368695',
-    outerbox: 'abb206c9-c795-4dad-bb04-74e94e8c3654',
-    ColorName: 'red',
-    SizeName: 'regular',
-    FactoryOrderId: 2,
-    CustomerOrderId: 'aee7514d-f202-4f9d-8212-97d9a459cbda' // 1
-  }});
-
-  /**********
-   * USERS
-   */
-  await Users.findOrCreate({where: {
-    firstname: 'Jonathan',
-    lastname: 'Chang',
-    email: 'j.a.chang820@gmail.com',
-    role: 'Administrator'
-  }});
-
-  await Users.findOrCreate({where: {
-    firstname: 'Alex',
-    lastname: 'Chen',
-    email: 'aqchen@g.ucla.edu',
-    role: 'Administrator'
-  }});
 }
 
 setup().then(() => {
   process.exit();
-}).catch(() => {
-  console.log('Error loading dummy data');
+}).catch((err) => {
+  console.log('Error loading dummy data.');
+  console.log(err);
   process.exit();
 });
