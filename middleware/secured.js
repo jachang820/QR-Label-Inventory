daef3a840c78ebdcb84546d6728c7a7717408afb
alexanderqchen <aqchen@ucla.edu>
2018-12-30 18:10:46 -0800

    fixed spacing

--- middleware/secured.js
+++ middleware/secured.js
@@ -3,32 +3,32 @@ const jwt = require('jsonwebtoken');
 
 /* Determine whether request was made from an axios call
    within the back-end. */
-   function serverRequest(req) {
-    let auth = req.headers.authorization;
-    if (auth && auth.startsWith("Basic ")) {
-      let base64 = auth.substring(6);
-      let pair = Buffer.from(base64, 'base64')
-                       .toString('ascii')
-                       .split(':');
-      if (pair.length === 2 && pair[0] === "server" &&
-        pair[1] === process.env.SERVER_SECRET) {
+function serverRequest(req) {
+  let auth = req.headers.authorization;
+  if (auth && auth.startsWith("Basic ")) {
+    let base64 = auth.substring(6);
+    let pair = Buffer.from(base64, 'base64')
+                     .toString('ascii')
+                     .split(':');
+    if (pair.length === 2 && pair[0] === "server" &&
+      pair[1] === process.env.SERVER_SECRET) {
 
-        return true;
-      }
-    } else {
-      return false;
+      return true;
     }
-   }
+  } else {
+    return false;
+  }
+}
 
 /* Verify that logged in user was authenticated and reaffirm
    the user's role. If user is invalid, log out. */
 module.exports = function() {
   return function secured (req, res, next) {
 
-    if (!serverRequest(req)) {    
+    if (!serverRequest(req)) {
 
       let secret = process.env.SECURITY_SECRET;
-      let token = req.user.token; 
+      let token = req.user.token;
 
       try {
         var decoded = jwt.verify(token, secret);
@@ -36,7 +36,7 @@ module.exports = function() {
         req.session.returnTo = req.originalUrl;
          return res.redirect('/auth/logout');
       }
-      
+
       res.locals.firstname = decoded.firstname;
       res.locals.lastname = decoded.lastname;
       res.locals.email = decoded.email;
